c2c4e41d27ffcc577e65fa389ed1777a81805618
MisterJerry123 <misterjerry12345@gmail.com>
2026-07-15 18:06:39 +0900

    test: RemoteExchangeDataSourceImpl 단위 테스트 추가
    - axios 모듈을 Jest mock(jest.mock)으로 처리하여 HTTP 요청 가로채기 적용
    - API 서버 성공 응답 및 500/네트워크 통신 에러 발생 시 예외 throw 동작 단위 테스트 작성
    - 호출된 베이스 API URL(BASE_URL) 및 호출 횟수 검증 적용

---
+++ __tests__/data/data_source/remoteDataSourceImpl.test.ts
@@ -0,0 +1,38 @@
+import axios from "axios"
+import { ExchangeInfoDto } from "../../../data/dto/exchangeInfoDto"
+import { RemoteExchangeDataSourceImpl } from "../../../data/data_source/remoteExchangeDataSourceImpl"
+
+//jest가 axios를 제어
+jest.mock("axios")
+const mockAxios = axios as jest.Mocked<typeof axios>
+
+describe("data Source 단위 테스트", () => {
+
+    beforeEach(() => {
+        jest.clearAllMocks()
+    })
+
+    test("getAllExchangeInfos는 API 서버로부터 데이터를 성공적으로 조회하여 DTO로 변환해야한다", async()  => {
+        //given
+        const mockDto: ExchangeInfoDto = {
+            amount: 1.0,
+            base: "EUR",
+            date: "2026-07-15",
+            rates: {
+                USD: 1.15,
+                KRW: 1750.0
+            }
+        }
+        mockAxios.get.mockResolvedValue({data:mockDto}) //axios.get 호출 시 mockDto를 리턴하도록 설정
+        const dataSource = new RemoteExchangeDataSourceImpl()
+
+        //when
+        const result = await dataSource.getAllExchangeInfos()
+
+        //then
+        expect(result).toEqual(mockDto)
+
+
+
+    })
+})
